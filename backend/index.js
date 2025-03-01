008a818cf96bfafcd86dad818598fd1180e716f4
Shivam Tiwari <shivamtiwarix88@gmail.com>
2025-03-01 09:02:30 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,25 @@
 import express from 'express';
-import hostname from "os";
+import os from "os"; 
 import cors from 'cors';
-import {router} from "./routes/index.js";
+import { router } from "./routes/index.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || "3000";
-
+const HOSTNAME = os.hostname();  
+app.options('*', cors());
 app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(`Request from ${req.url} ${req.hostname}`);
     next();
-})
-app.use('/api/v1', router);
-
-
-app.listen(PORT, hostname, (req, res)=> {
-    console.log(`Listening on ${hostname.machine} at ${PORT}`);
 });
 
+app.use('/api/v1', router);
 
-
-
-
-
+// Corrected app.listen syntax
+app.listen(PORT, () => {
+    console.log(`Listening on ${HOSTNAME} at port ${PORT}`);
+});
